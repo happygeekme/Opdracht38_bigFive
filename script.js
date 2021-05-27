f4a0dba33ee58aa5c341acf8e194bec472148e65
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2021-05-27 09:36:15 +0200

    opdracht bigFive antwoord

---
+++ script.js
@@ -0,0 +1,40 @@
+// opdracht 1
+const animalButtons = document.getElementsByClassName("big-five-button");
+const arrayBigFive = Array.from(animalButtons);
+const spottedAnimals = document.getElementById("spotted-animals-list");
+
+arrayBigFive.forEach(function(animal){
+        animal.addEventListener("click", addToSpottedList);
+})
+
+function addToSpottedList(event){
+        const index = (arrayBigFive.indexOf(event.target));
+        console.log(index);
+        const animal = arrayBigFive[index].textContent;
+        const newLi = document.createElement("li");
+        const node = document.createTextNode(animal);
+        newLi.appendChild(node);
+        newLi.classList.add("spotted-animals-list-item");
+        spottedAnimals.appendChild(newLi);
+}
+
+
+// opdracht 2
+const removeFirstItemBtn = document.getElementById("remove-first-item-button");
+
+removeFirstItemBtn.addEventListener("click", removeFirst);
+
+function removeFirst(){
+        if (spottedAnimals.hasChildNodes()){
+        spottedAnimals.removeChild(spottedAnimals.childNodes[0]);
+        };
+}
+
+// opdracht 3
+const removeAllBtn = document.getElementById("remove-all-button");
+
+removeAllBtn.addEventListener("click", removeAll);
+
+
+
+
